bb7e7ce8a7fdc1b5db90e8ed6b525bc5b72b2bbb
Jacob <jacobmgevans@gmail.com>
2018-05-03 20:56:36 -0700

    tests are not running on mock data I believe refactored for async but tests cant check if working properly

--- routes/user/profile-router.js
+++ routes/user/profile-router.js
@@ -8,35 +8,54 @@ const Profile = require('../../model/user/profile.js');
 const User = require('../../model/user/user.js');
 const bearerAuth = require('../../lib/bearer-auth-middleware.js');
 
-const profileRouter = module.exports = Router();
+const profileRouter = (module.exports = Router());
 
 // get current users profile
 // http GET :3000/api/profiles/currentuser 'Authorization:Bearer TOKEN'
-profileRouter.get('/api/profiles/currentuser', bearerAuth, (req, res, next) => {
-  Profile.findOne({userID: req.user._id})
-    .then(profile => {
-      if(!profile)
+profileRouter.get(
+  '/api/profiles/currentuser',
+  bearerAuth,
+  async (req, res, next) => {
+    try {
+      Profile.findOne({ userID: req.user._id });
+      if (!profile)
         return next(createError(404, 'NOT FOUND ERROR: profile not found'));
-      res.json(profile);
-    })
-    .catch(next);
-});
+      const profileCurrentUser = await res.json(profile);
+    } catch (next) {
+      console.log(next);
+    }
+  }
+);
 
 // update profile
 // http PUT :3000/api/profile/:profileID 'Authorization:Bearer TOKEN' username='new username'
-profileRouter.put('/api/profile/:profileID', bearerAuth, json(), (req, res, next) => {
-  debug('PUT: /api/profile:profileID');
+profileRouter.put(
+  '/api/profile/:profileID',
+  bearerAuth,
+  json(),
+  async (req, res, next) => {
+    try {
+      debug('PUT: /api/profile:profileID');
+      req.body.lastLogin = new Date();
 
-  req.body.lastLogin = new Date();
-  Profile.findByIdAndUpdate(req.params.profileID, req.body, {new: true, runValidators: true})
-    .then(myProfile => {
-      if(!myProfile)
+      const findProfileIDThenUpdate = await Profile.findByIdAndUpdate(
+        req.params.profileID,
+        req.body,
+        {
+          new: true,
+          runValidators: true
+        }
+      );
+      if (!findProfileIDThenUpdate) {
         return next(createError(404, 'NOT FOUND ERROR: profile not found'));
-        
-      let usernameObj = {username: myProfile.username };
-      User.findByIdAndUpdate(myProfile.userID, usernameObj, {runValidators: true})
-        .then(() => res.json(myProfile))
-        .catch(next);
-    })
-    .catch(next);
-});
+      }
+      const myProfile = await { username: findProfileIDThenUpdate.username };
+      User.findByIdAndUpdate(findProfileIDThenUpdate.userID, usernameObj, {
+        runValidators: true
+      });
+      const resolving = await res.json(myProfile);
+    } catch (next) {
+      console.log(next);
+    }
+  }
+);
